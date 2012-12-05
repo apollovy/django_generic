43d023797e689bed3f1b0b91f243a6d10fb71ff3
apollov <apollov@greenlabs.ru>
2012-12-05 19:09:23 +0400

    Return only published in past objects in PublishedManager.published.
    Add published_future method to PublishedManager.
    
    Signed-off-by: apollov <apollov@greenlabs.ru>

--- apollov/django/apps/generic/managers.py
+++ apollov/django/apps/generic/managers.py
@@ -3,9 +3,20 @@ Created on 14.11.2012
 
 @author: apollov
 '''
+from datetime import datetime
+
 from django.db.models import Manager
 
 
 class PublishedManager(Manager):
     def published(self):
-        return self.get_query_set().filter(is_published=True)
+        return self.get_query_set().filter(
+           is_published=True,
+           publish_date_time__lte=datetime.now(),
+       )
+
+    def published_future(self):
+        return self.get_query_set().filter(
+           is_published=True,
+           publish_date_time__gt=datetime.now(),
+       )
