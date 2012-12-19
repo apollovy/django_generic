decbad18ee3aaeebe28236428e1843c6827af004
apollov <apollov@greenlabs.ru>
2012-12-19 19:08:24 +0400

    Make only published object appear in index of PublishedSearchIndexMixin. Add method and decorator function to remove unpublished objects on save.
    #FIXME: bulk-publishing/unpublishing doesn't remove objects from index
    due to save events not fired. Standart django behaviour.
    
    Signed-off-by: apollov <apollov@greenlabs.ru>

--- apollov/django/apps/generic/search_indexes.py
+++ apollov/django/apps/generic/search_indexes.py
@@ -5,6 +5,8 @@ Created on 10.12.2012
 '''
 import datetime
 
+from django.db.models import signals
+
 from haystack import indexes
 
 
@@ -14,10 +16,23 @@ class TranslationSearchIndexMixin(indexes.SearchIndex):
 
 
 class PublishedSearchIndexMixin(indexes.SearchIndex):
-    publish_date_time = indexes.DateTimeField(model_attr='publish_date_time')
+    publish_date_time = indexes.DateTimeField(
+                                          model_attr='publish_date_time')
     is_published = indexes.BooleanField(model_attr='is_published')
 
-    def read_queryset(self):
+    def index_queryset(self):
         return super(PublishedSearchIndexMixin, self).index_queryset()\
             .filter(publish_date_time__lte=datetime.datetime.now(),
                     is_published=True)
+
+    def remove_object_if_unpublished(self, instance, **kwargs):
+        if instance.is_published == False:
+            self.remove_object(instance, **kwargs)
+
+
+def remove_object_if_unpublished(func):
+    def _setup_save(self, model):
+        func(self, model)
+        signals.post_save.connect(self.remove_object_if_unpublished,
+                                  sender=model)
+    return _setup_save
