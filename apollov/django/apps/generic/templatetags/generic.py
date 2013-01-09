d4354139b0cbb08ce53b6cbc320ff8b958467a1b
apollov <apollov@greenlabs.ru>
2013-01-09 18:31:07 +0400

    Add basename template tag.
    Signed-off-by: apollov <apollov@greenlabs.ru>

--- apollov/django/apps/generic/templatetags/generic.py
+++ apollov/django/apps/generic/templatetags/generic.py
@@ -3,7 +3,7 @@ Created on 04.12.2012
 
 @author: apollov
 '''
-from os.path import splitext as _splitext
+from os.path import splitext as _splitext, basename as _basename
 
 from django import template
 
@@ -13,3 +13,8 @@ register = template.Library()
 @register.filter
 def splitext(value):
     return _splitext(value)
+
+
+@register.filter
+def basename(value):
+    return _basename(value)
